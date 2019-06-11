ca79b4bbc7b724d12410e16febb5fa2dc189696e
hongjuns <asws12345@naver.com>
2019-06-11 11:09:16 +0900

    commit0611

--- app/src/main/java/com/example/dss/fragment1.java
+++ app/src/main/java/com/example/dss/fragment1.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -23,6 +24,8 @@ public class fragment1 extends Fragment {
 
     ListViewAdapter adapter;
     EditText editText;
+    CheckBox cb1;
+    CheckBox cb2;
 
     static final String ImageUrl = "http://211.239.124.237:19609/resources/big_image/";
 
@@ -32,6 +35,8 @@ public class fragment1 extends Fragment {
         View view = inflater.inflate(R.layout.fragment1, null);
         ListView listview = (ListView) view.findViewById(R.id.listview);
         editText = (EditText) view.findViewById(R.id.edit1);
+        cb1 = (CheckBox)view.findViewById(R.id.checkBox1);
+        cb2 = (CheckBox)view.findViewById(R.id.checkBox2);
 
         Button btn_server = (Button) view.findViewById(R.id.but1);
 
@@ -41,8 +46,16 @@ public class fragment1 extends Fragment {
 
                 String Drug = editText.getText().toString();
                 Activity root = getActivity(); //이 클래스가 프레그먼트이기 때문에 액티비티 정보를 얻는다.
-                Toast.makeText(root, Drug, Toast.LENGTH_SHORT).show();
-                loadData(Drug);
+
+                if (cb1.isChecked() == true){
+                    loadData(Drug);
+                }else if (cb2.isChecked() == true){
+                    loadData2(Drug);
+                }else{
+                    loadData(Drug);
+                }
+
+
             }
         });
         adapter = new ListViewAdapter(this.getActivity());
@@ -57,6 +70,11 @@ public class fragment1 extends Fragment {
                 intent.putExtra("validity", item.getValidity());
                 intent.putExtra("manufacturing", item.getManufacturing());
                 intent.putExtra("usage", item.getUsage());
+
+                intent.putExtra("name", item.getName());
+                intent.putExtra("company_name", item.getCompany_name());
+                intent.putExtra("big_image",item.getBig_image());
+
                 startActivity(intent);//액티비티 띄우기
             }
         });
@@ -64,6 +82,7 @@ public class fragment1 extends Fragment {
         return  view;
 
     }
+    //약이름 검색 API 호출
     private void loadData(String Drug) {
         AQuery aq = new AQuery(getActivity());
 
@@ -102,6 +121,57 @@ public class fragment1 extends Fragment {
 
                             arItem.add(item);
                         }
+                        if(arItem.size()>0){
+                            adapter.getArItem().addAll(arItem);
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                    catch (Exception e){
+                        Toast.makeText(getActivity(), "JSON Parsing 오류 발생", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    //fail
+                    Toast.makeText(getActivity(), "잘못된 요청입니다", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }.timeout(20000));
+    }
+
+    //약ID 검색 API 호출
+    private void loadData2(String Drug) {
+        AQuery aq = new AQuery(getActivity());
+
+        String url = "http://211.239.124.237:19613/drug/findId/"+Drug;
+
+
+        aq.ajax(url, JSONObject.class, new AjaxCallback<JSONObject>() {
+            @Override
+            public void callback(String url, JSONObject jsonObject, AjaxStatus status) {
+                Log.i ("url",url);
+                if (jsonObject != null) {
+                    //sucess
+                    Log.i("test", jsonObject.toString());
+                    try {
+                        ArrayList<ListViewItemApi> arItem = new ArrayList<ListViewItemApi>();
+
+                        String str = jsonObject.getString("big_image");
+                        String[] array = str.split("/");
+                        String newurl = ImageUrl + array[6] + ".jpg";
+
+                        ListViewItemApi item = new ListViewItemApi();
+                        item.setName(jsonObject.getString("name"));
+                        item.setCompany_name(jsonObject.getString("company_name"));
+                        item.setDiv_name(jsonObject.getString("div_name"));
+
+                        item.setIngredient_detail(jsonObject.getString("ingredient_detail"));
+                        item.setUsage(jsonObject.getString("usage"));
+                        item.setValidity(jsonObject.getString("validity"));
+                        item.setManufacturing(jsonObject.getString("manufacturing"));
+
+                        item.setBig_image(newurl);
+
+                        arItem.add(item);
+
                         if(arItem.size()>0){
                             adapter.getArItem().addAll(arItem);
                             adapter.notifyDataSetChanged();
--- app/src/main/java/com/example/dss/fragment5.java
+++ app/src/main/java/com/example/dss/fragment5.java
@@ -1,9 +1,25 @@
 package com.example.dss;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class fragment5 extends AppCompatActivity  {
 
@@ -11,6 +27,13 @@ public class fragment5 extends AppCompatActivity  {
     String validity;
     String manufacturing;
     String usage;
+    String name;
+    String company_name;
+    String big_image;
+    private  WebView mwv;
+    ImageView imView;
+    Bitmap bmImg;
+    back task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +45,53 @@ public class fragment5 extends AppCompatActivity  {
         validity = Intent.getExtras().getString("validity");
         manufacturing = Intent.getExtras().getString("manufacturing");
         usage = Intent.getExtras().getString("usage");
+        name = Intent.getExtras().getString("name");
+        company_name = Intent.getExtras().getString("company_name");
+        big_image = Intent.getExtras().getString("big_image");
+
+      //  mwv=(WebView)findViewById(R.id.WebView);
+      //  WebSettings mws=mwv.getSettings();//Mobile Web Setting
+       // mws.setJavaScriptEnabled(true);//자바스크립트 허용
+        //mws.setLoadWithOverviewMode(true);//컨텐츠가 웹뷰보다 클 경우 스크린 크기에 맞게 조정
+       // mwv.loadUrl("http://211.239.124.237:19609/resources/big_image/1MfSEl6hMOG.jpg");
 
         TextView textView = (TextView) findViewById(R.id.View1);
         TextView textView2 = (TextView) findViewById(R.id.View2);
         TextView textView3 = (TextView) findViewById(R.id.View3);
+        TextView textView4 = (TextView) findViewById(R.id.View4);
+        TextView textView5 = (TextView) findViewById(R.id.View5);
 
         textView.setText(ingredient_detail);
         textView2.setText(validity);
         textView3.setText(manufacturing);
+        textView4.setText(name);
+        textView5.setText(company_name);
+        imView = (ImageView) findViewById(R.id.imageView1);
+
+        task = new back();
+        task.execute(big_image);
 
     }
+    private class back extends AsyncTask<String, Integer,Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            // TODO Auto-generated method stub
+            try{
+                URL myFileUrl = new URL(urls[0]);
+                HttpURLConnection conn = (HttpURLConnection)myFileUrl.openConnection();
+                conn.setDoInput(true);
+                conn.connect();
+                InputStream is = conn.getInputStream();
+                bmImg = BitmapFactory.decodeStream(is);
 
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            return bmImg;
+        }
+        protected void onPostExecute(Bitmap img){
+            imView.setImageBitmap(bmImg);
+        }
+    }
 
 }
